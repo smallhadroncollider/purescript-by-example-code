6da8010e00b2134c20f824daca8037aa138cc785
Mark Wales <mark@smallhadroncollider.com>
2016-06-16 10:44:29 +0100

    added early examples

--- chapter-4/src/Main.purs
+++ chapter-4/src/Main.purs
@@ -1,9 +1,21 @@
 module Main where
 
 import Prelude
-import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Console (CONSOLE, log)
+import Data.Array
+import Data.Foldable
+import Control.Monad.Eff.Console (infoShow)
 
-main :: forall e. Eff (console :: CONSOLE | e) Unit
-main = do
-  log "Hello sailor!"
+main = infoShow (fib 10)
+
+fib :: Int -> Int
+fib 1 = 1
+fib 2 = 1
+fib n = fib (n - 1) + fib (n - 2)
+
+-- Generates an array of all possible number pairs from 1 to n
+pairs :: Int -> Array (Array Int)
+pairs n = concatMap (\i -> map (\j -> [i, j]) (i .. n)) (1 .. n)
+
+-- Tests number pairs for factors 
+factors :: Int -> Array (Array Int)
+factors n = filter (\pair -> product pair == n)(pairs n)
